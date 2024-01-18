22a5e2afe216d8cb5c8c175e0effcaff8f569549
arnau <arnau@DESKTOP-02LQMB1>
2024-01-18 20:52:09 +0100

    Agregar archivos de proyecto.

---
+++ Functions/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Functions
+{
+    public class Class1
+    {
+       
+
+    }
+}
---
+++ PR1. Herois vs Monstre V2/Program.cs
@@ -0,0 +1,12 @@
+﻿using Functions;
+namespace HeroisVsMonstreV2
+{
+    public class HeroisVsMonstreV2Proj
+    {
+        public static void Main()
+        {
+            
+
+        }
+    }
+}
