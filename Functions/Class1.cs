e6b39287394b006e2b7229b21ca7ada66b4250c6
arnau <arnau@DESKTOP-02LQMB1>
2024-01-23 22:05:49 +0100

    Added input SelectorDificultat Closes #2

--- Functions/Class1.cs
+++ Functions/Class1.cs
@@ -55,6 +55,31 @@ namespace Functions
         }
 
 
+        //selector de dificultat
+        public static int DificultSelector(int dificultat)
+        {
+            Console.Clear();
+            int intents = 3;
+
+            do
+            {
+                Console.WriteLine("Introdueix a quina dificultat vols jugar:");
+                Console.WriteLine("1.Facil");
+                Console.WriteLine("2.Dificil");
+                Console.WriteLine("3.Personalitzat");
+                Console.WriteLine("4.Random");
+                dificultat = Convert.ToInt32(Console.ReadLine());
+                if (dificultat > 4 || dificultat < 1) { Console.WriteLine("Recorda introduir els valors dins del rang!"); intents--; }
+
+            } while ((dificultat > 4 || dificultat < 1) && intents > 0);
+
+            if (intents == 0) { Console.WriteLine("Fins una altra!"); return intents; }
+
+            return dificultat;
+        }
+
+
+
 
 
 
