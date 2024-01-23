d8b2b092c481eeb2ac42dc794f14db2aa3038f5d
arnau <arnau@DESKTOP-02LQMB1>
2024-01-23 22:00:14 +0100

    Added input Menu Closes #11

--- Functions/Class1.cs
+++ Functions/Class1.cs
@@ -4,7 +4,44 @@ namespace Functions
 {
     public class Class1
     {
-       
+
+        private static int CountHB = 0;
+        private static int auxRedP2 = 0;
+
+
+
+        //menu jugar o sortir
+        public static int Menu(int choose)
+        {
+            string Bye = "D'acord, adeu!";
+            int intents = 3; choose = 0;
+
+            do
+            {
+                
+                Console.WriteLine("1.Jugar 0.Sortir:");
+                choose = Convert.ToInt32(Console.ReadLine());
+                if (choose > 1 || choose < 0)
+                {
+                    intents--;
+                    Console.WriteLine("Recorda introduir els valors dins del rang!");
+                }
+
+            } while ((choose > 1 || choose < 0) && intents > 0);
+
+            if (intents == 0)
+            {
+                Console.WriteLine("Fins una altra!");
+                return intents;
+            }
+            if (choose == 0) { Console.WriteLine(Bye); }
+
+            return choose;
+        }
+
+
 
     }
+
 }
+
